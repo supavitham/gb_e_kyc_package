b32a9891ce613e04a02e5f05c0f65ed37479ee5f
supavitham <supavitham@gmail.com>
2022-05-24 22:28:12 +0700

    .

--- lib/gb_e_kyc_package.dart
+++ lib/gb_e_kyc_package.dart
@@ -8,15 +8,15 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Calculator {
   /// Returns [value] plus 1.
   Widget addOne() {
-    loadENV();
+    // loadENV();
     return Lottie.network('https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json');
   }
 
-  loadENV() {
-    dotenv.load(fileName: "assets/.env");
-    String? hostRegister = dotenv.env['host3003'];
-    String? hostGateway = dotenv.env['host3006'];
-    String? authorization2 = dotenv.env['authorization2'];
-    print("hostRegister>>>> $hostRegister -- $hostGateway -- $authorization2");
-  }
+  // loadENV() {
+  //   dotenv.load(fileName: "assets/.env");
+  //   String? hostRegister = dotenv.env['host3003'];
+  //   String? hostGateway = dotenv.env['host3006'];
+  //   String? authorization2 = dotenv.env['authorization2'];
+  //   print("hostRegister>>>> $hostRegister -- $hostGateway -- $authorization2");
+  // }
 }
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:gb_e_kyc_package/gb_e_kyc_package.dart';
+
+void main() async {
+  await dotenv.load(fileName: "assets/.env");
+
+  String? hostRegister = dotenv.env['host3003'];
+  String? hostGateway = dotenv.env['host3006'];
+  String? authorization2 = dotenv.env['authorization2'];
+  print("hostRegister>>>> $hostRegister -- $hostGateway -- $authorization2");
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Calculator().addOne());
+  }
+}
