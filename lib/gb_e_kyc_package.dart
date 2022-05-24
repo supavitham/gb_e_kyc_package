40054c7a4019f5006696fc6e97e2687c071cad19
supavitham <supavitham@gmail.com>
2022-05-24 17:05:23 +0700

    first commit

---
+++ lib/gb_e_kyc_package.dart
@@ -0,0 +1,9 @@
+library gb_e_kyc_package;
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  Widget addOne() => Lottie.network('https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json');
+}
