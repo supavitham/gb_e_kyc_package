dec8785209b321ca52c1c7bd1c8be24d213ef0a6
supavitham <supavitham@gmail.com>
2022-05-25 10:58:05 +0700

    .

---
+++ lib/e_kyc_button_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:gb_e_kyc_package/main.dart';
+
+class EKYCButtonWidget extends StatefulWidget {
+  final BoxDecoration? boxDecorationContainer;
+  final String? title;
+  final TextStyle? textStyle;
+  final double? heightContainer;
+  final double? widthContainer;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+
+  const EKYCButtonWidget({Key? key, this.boxDecorationContainer, this.title, this.textStyle, this.heightContainer, this.widthContainer, this.padding, this.margin}) : super(key: key);
+
+  @override
+  State<EKYCButtonWidget> createState() => _EKYCButtonWidgetState();
+}
+
+class _EKYCButtonWidgetState extends State<EKYCButtonWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const MainAppEKyc()),
+      ),
+      child: Container(
+        width: widget.widthContainer,
+        height: widget.heightContainer,
+        padding: widget.padding == null ? EdgeInsets.symmetric(vertical: 16, horizontal: 8) : widget.padding,
+        margin: widget.margin == null ? EdgeInsets.all(8) : widget.margin,
+        decoration: widget.boxDecorationContainer == null
+            ? BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              )
+            : widget.boxDecorationContainer,
+        child: Center(
+          child: Text(
+            "${widget.title ?? "KYC"}",
+            style: widget.textStyle == null ? TextStyle(fontSize: 20, color: Colors.black) : widget.textStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/gb_e_kyc_package.dart
+++ lib/gb_e_kyc_package.dart
@@ -1,23 +1,22 @@
 library gb_e_kyc_package;
 
 import 'package:flutter/material.dart';
-import 'package:gb_e_kyc_package/main.dart';
-import 'package:lottie/lottie.dart';
+import 'package:gb_e_kyc_package/e_kyc_button_widget.dart';
 
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   Widget addOne() {
     // loadENV();
-    return const MainAppEKyc();
+    return const EKYCButtonWidget();
     // return Lottie.network('https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json');
   }
 
-  // loadENV() {
-  //   dotenv.load(fileName: "assets/.env");
-  //   String? hostRegister = dotenv.env['host3003'];
-  //   String? hostGateway = dotenv.env['host3006'];
-  //   String? authorization2 = dotenv.env['authorization2'];
-  //   print("hostRegister>>>> $hostRegister -- $hostGateway -- $authorization2");
-  // }
+// loadENV() {
+//   dotenv.load(fileName: "assets/.env");
+//   String? hostRegister = dotenv.env['host3003'];
+//   String? hostGateway = dotenv.env['host3006'];
+//   String? authorization2 = dotenv.env['authorization2'];
+//   print("hostRegister>>>> $hostRegister -- $hostGateway -- $authorization2");
+// }
 }
